a1d0350802f0f57f68123d22965c4dc5cae4c5a2
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-13 22:31:29 +0530

    fourth commit

---
+++ src/com/internshala/operator1.java
@@ -0,0 +1,10 @@
+package com.internshala;
+public class operator1 {
+	public static void main(String[] args) {
+		int x = 50;
+		++x;
+		System.out.println(x);
+		--x;
+		System.out.println(x);
+	}
+}
