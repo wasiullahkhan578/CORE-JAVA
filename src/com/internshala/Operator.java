0b9a3c25fd6426fecdf793cf44cadee81a4ff625
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-13 22:18:20 +0530

    third commit

---
+++ src/com/internshala/Operator.java
@@ -0,0 +1,16 @@
+package com.internshala;
+public class Operator {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int sum = num1 +num2;
+		System.out.println(sum);
+		int sub = num1-num2;
+		System.out.println(sub);
+		int mul = num1 * num2;
+		System.out.println(mul);
+		int div = num2/num1;
+		System.out.println(div);
+	}
+
+}
