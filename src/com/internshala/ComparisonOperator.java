ef23a022bda312914bb104874334bdae007a0d82
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-14 11:23:13 +0530

    ComparisonOperator

---
+++ src/com/internshala/ComparisonOperator.java
@@ -0,0 +1,22 @@
+package com.internshala;
+public class ComparisonOperator {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		//+=
+		num1+=num2;
+		System.out.println(num1);
+		//-=
+		num1-=num2;
+		System.out.println(num1);
+		//*=
+		num1*=num2;
+		System.out.println(num1);
+		///=
+		num1/=num2;
+		System.out.println(num1);
+		
+
+	}
+
+}
